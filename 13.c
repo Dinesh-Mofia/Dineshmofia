e7f6aca54d598cc8b5e71015dce510731e6300e8
Dinesh-Mofia <30723628+Dinesh-Mofia@users.noreply.github.com>
2017-08-11 17:16:44 +0530

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+ 
+int main() {
+  int num, i, isP=0;
+  printf("Enter a positive number\n");
+  scanf("%d",&num);
+
+  for(i = 2; i <=(num/2); ++i) {
+      if(num%i==0) {
+          isP=1;
+          break;
+      }
+  }
+   
+  if(isPrime==0)
+      printf("%d is a Prime Number",num);
+  else
+      printf("%d is NOT a Prime Number",num);
+       
+  getch();
