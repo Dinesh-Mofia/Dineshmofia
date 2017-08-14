5e95f957031f558ccfb4c9b53b3ca745a0f48f1b
Dinesh-Mofia <30723628+Dinesh-Mofia@users.noreply.github.com>
2017-08-14 10:07:37 +0530

    Update 10.c

--- 10.c
+++ 10.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 int main()
 {
-    long long n;
-    int count = 0;
+    long long k;
+    int c = 0;
 
     printf("Enter an integer: ");
-    scanf("%lld", &n);
+    scanf("%lld", &k);
 
-    while(n != 0)
+    while(k != 0)
     {
         // n = n/10
-        n /= 10;
-        ++count;
+        k /= 10;
+        ++c;
     }
 
-    printf("Number of digits: %d", count);
+    printf("Number of digits: %d", c);
 }
 
 
